6d2716a102f734f58d933eb88577b205bd40d1ea
Nate Davis Olds <nate@davisolds.com>
2014-12-09 20:27:42 -0500

    initial port over from prism

---
+++ lib/factoric/base.rb
@@ -0,0 +1,91 @@
+module Factoric
+  module Base
+    def self.included base
+      base.extend ClassMethods
+    end
+
+    module ClassMethods
+      def find id, options={}
+        fact_class = options.fetch(:fact_class) { HistoricalDetail }
+        timestamp = options.fetch(:at) { DateTime.now }
+
+        facts = fact_class.find_before id, timestamp
+
+        build id, facts
+      end
+
+      def build_from_details details=[]
+        facts = [*details]
+
+        if facts.length > 0
+          id = facts.first.entity_id
+          build id, facts
+        end
+      end
+
+      def build id, facts
+        new id, facts
+      end
+
+      def has_many_entities entity, options={}
+        singular = entity.to_s.singularize
+        entity_class = options.fetch(:entity_class, singular.classify.constantize)
+        plural = entity.to_s.pluralize
+        instance_var = "@#{plural}"
+
+        define_method singular do |id|
+          send(plural).detect { |v| v.id == id.to_i }
+        end
+
+        define_method plural do
+          if instance_variable_defined?(instance_var)
+            instance_variable_get(instance_var)
+          else
+            entities = build_entities entity_class
+            instance_variable_set(instance_var, entities)
+            entities
+          end
+        end
+      end
+    end
+
+    attr_reader :id, :specifics, :entity_facts
+
+    def initialize id, facts
+      @id = id
+      @entity_facts = {}
+
+      facts.group_by(&:entity).each do |entity, related_facts|
+        entity_name = entity.present? ? entity.downcase : "household"
+
+        @entity_facts[entity_name] = [] unless @entity_facts.has_key? entity_name
+        @entity_facts[entity_name] += related_facts
+      end
+
+      @specifics = facts_by_fact_key entity_facts.fetch("household", {})
+    end
+
+    def build_entities historic_entity
+      entities = []
+
+      facts = entity_facts.fetch(historic_entity.param_key, [])
+
+      facts.group_by(&:entity_id).each do |entity_id, details|
+        entity = historic_entity.build(entity_id, details, self)
+        entities << entity if entity.present?
+      end
+
+      entities.sort { |a,b| a.id <=> b.id }
+    end
+
+    def facts_by_fact_key facts=[]
+      results = {}
+
+      facts.group_by(&:fact_key).each do |fact_key, grouped_facts|
+        results[fact_key] = grouped_facts.sort { |a,b| b.happened_at <=> a.happened_at }
+      end
+
+      results
+    end
+  end
+end
---
+++ lib/factoric/entity.rb
@@ -0,0 +1,172 @@
+require 'bigdecimal'
+require 'date'
+
+module Factoric
+  module Entity
+    def self.included(base)
+      base.extend ClassMethods
+    end
+
+    module ClassMethods
+      CONVERTERS = {
+        "string" => Proc.new { |v| v || "" },
+        "symbol" => Proc.new { |v| v.blank? ? nil : v.to_sym },
+        "integer" => Proc.new { |v| v.to_i },
+        "amount" => Proc.new { |v|
+          v = 0 if v.nil?
+          BigDecimal.new(v).round(2)
+        },
+        "boolean" => Proc.new { |v| v == "true" || v == "1"},
+        "datetime" => Proc.new { |v| DateTime.parse(v) rescue nil },
+        "custom" => Proc.new { |v, &block| block.call(v) }
+      }
+
+      def model_name
+        self
+      end
+
+      def param_key
+        model_name.to_s.downcase
+      end
+
+      def build_from_details details=[], household=nil
+        facts = [*details]
+
+        if facts.length > 0
+          id = facts.first.entity_id
+          build id, facts, household
+        end
+      end
+
+      def build id, details, household=nil
+        return nil if details.any? &:forgotten_all?
+
+        facts = {}
+
+        details.group_by(&:fact_key).each do |fact_key, grouped_facts|
+          facts[fact_key] = grouped_facts.sort { |a,b| b.happened_at <=> a.happened_at }
+        end
+
+        new id, facts, household
+      end
+
+      def attr_collection name, options={}
+        converter_key = options.fetch(:convert_to, "string")
+        converter = CONVERTERS.fetch(converter_key) { converters["string"] }
+        ignored_value = options.fetch(:ignore_value, nil)
+        self.historical_detail_collection_keys << name
+
+        define_method name do
+          collection = []
+
+          relevant_specifics(name.to_s).group_by(&:value).each do |value, facts|
+            recent_fact = facts.first
+            collection << converter.call(recent_fact.value) unless recent_fact.forgotten? || recent_fact.value == ignored_value
+          end
+
+          collection.compact.uniq.sort{|a, b| a <=> b}
+        end
+      end
+
+      def historical_detail_fact_keys
+        @historical_detail_fact_keys ||= []
+      end
+
+      def historical_detail_collection_keys
+        @historical_detail_collection_keys ||= []
+      end
+
+      def fact name, options={}, &block
+        default_value = options.fetch(:default, nil)
+        converter_key = options.fetch(:convert_to, "string")
+        use_default_for_value = options.fetch(:use_default_for, nil)
+        converter = CONVERTERS.fetch(converter_key) { converters["string"] }
+        instance_var = "@#{name}"
+        self.historical_detail_fact_keys << name
+
+        define_method name do
+          if instance_variable_defined?(instance_var)
+            instance_variable_get(instance_var)
+          else
+            facts = [*specifics[name.to_s]]
+            return default_value if facts.empty?
+
+            first_fact = facts.first
+            value = if first_fact.forgotten? || first_fact.value == use_default_for_value
+              default_value
+            else
+              converter.call(first_fact.value, &block)
+            end
+
+            instance_variable_set(instance_var, value)
+          end
+        end
+      end
+
+      def redefine_fact key, alias_key="fact_#{key}".to_sym, &block
+        alias_method alias_key, key
+        define_method key, &block
+      end
+    end
+
+    def initialize id, specifics={}, household=nil
+      @id = id
+      @specifics = {}
+      @household = household
+
+      specifics.each { |k, v| @specifics[k.to_s] = v }
+    end
+
+    def relevant_specifics fact_key
+      [*specifics[fact_key]].take_while do |detail|
+        !detail.forgotten_key?
+      end
+    end
+
+    def inspect
+      vars = [:@id].map {|v| "#{v}=#{instance_variable_get(v)}" }
+      vars += self.class.historical_detail_fact_keys.collect do |v|
+        value = send(v)
+
+        if value.present?
+          "#{v}=#{value}"
+        end
+      end.compact
+
+      "<#{self.class}: #{vars.join(", ") }>"
+    end
+
+    def id
+      @id
+    end
+    alias :to_param :id
+
+    def to_key
+      [*id]
+    end
+
+    def specifics
+      @specifics
+    end
+
+    def household
+      @household
+    end
+
+    def == candidate
+      return false if candidate.nil?
+      candidate_id = candidate.respond_to?(:id) ? candidate.id : candidate
+      id == candidate_id
+    end
+    alias :eql? :==
+
+    def != candidate
+      !(self.== candidate)
+    end
+
+    def hash
+      id.hash
+    end
+  end
+
+end
